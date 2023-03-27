399d998ce645ce4734ccf9c0929e2e2abdec818a
rakeshreddylokepalli <129029604+rakeshreddylokepalli@users.noreply.github.com>
2023-03-27 10:39:23 +0530

    Add files via upload

---
+++ day2Program09.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class program09
+{
+public static void main(String args[])
+{
+Scanner scob=new Scanner(System.in);
+int num=scob.nextInt();//1
+switch(num)
+{
+case 1:{System.out.print("ROCKY");}
+case 2:{System.out.print("SAI");}
+case 3:{System.out.print("THARUN");}
+case 4:{System.out.print("MANJU");}
+case 5:{System.out.print("VEERA GOPAL");}
+case 6:{System.out.print("SAMEER");}
+case 7 :{System.out.print("MAHESH");}
+default{System.out.print("INVALID");}
+}//main()
+}//progarm09
+
+
+
+
+
+
+
