9dece98ea91b251e3a42b6cf4b2dd81f487e165e
rakeshreddylokepalli <129029604+rakeshreddylokepalli@users.noreply.github.com>
2023-03-27 10:37:26 +0530

    Add files via upload

---
+++ day2Program06.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+class program06
+{
+public static void main(String args[])
+{
+Scanner scob=new Scanner(System.in);
+int num=scob.nextInt();//
+if(num==1){System.out.print("ROCKY");}
+else if(num==2){System.out.print("SAI");}
+else if(num==3){System.out.print("THARUN");}
+else if(num==4){System.out.print("MANJU");}
+else if(num==5){System.out.print("VEERA GOPAL");}
+else if(num==6){System.out.print("SAMEER");}
+else if(num==7){System.out.print("MAHESH");}
+else {System.out.print("INVALID");}
+}//main()
+}//progarm0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
