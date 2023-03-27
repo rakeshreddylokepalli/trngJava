4dbeca0c1019b47aaa3d0acdc0c16d803384654d
rakeshreddylokepalli <129029604+rakeshreddylokepalli@users.noreply.github.com>
2023-03-27 10:32:53 +0530

    Add files via upload

---
+++ day2Program01.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+class program14
+{
+public static void main(String args[])
+{
+Scanner scob=new Scanner(System.in);
+int num1=scob.nextInt();//5
+int i;//variable declaration
+i=0;//initialization
+while (i<=10)
+{
+ 	System.out.println(num1+"*"+i+"="+num1*i);
+	i=i+1;
+}//while loop
+
+}//main()
+}//progarm11
+//+,-,*,/,% 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
