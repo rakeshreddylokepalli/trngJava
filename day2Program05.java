14fe11abe79485ab5e18066ec4e55e2ca0fdaf91
rakeshreddylokepalli <129029604+rakeshreddylokepalli@users.noreply.github.com>
2023-03-27 10:37:02 +0530

    Add files via upload

---
+++ day2Program05.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+class program05
+{
+public static void main(String args[])
+{
+Scanner scob=new Scanner(System.in);
+int num=scob.nextInt();//5214
+if(num%2==0){System.out.print("EVEN");}
+if(num%2 !=0){System.out.print("ODD");}
+}//main()
+}//progarm05
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
